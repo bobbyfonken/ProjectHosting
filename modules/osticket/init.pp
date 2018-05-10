3818ec4d536e16ebfea84b4ddd6438db5dfc97a3
bobbyfonken <33414259+bobbyfonken@users.noreply.github.com>
2018-05-10 21:35:43 +0200

    Create init.pp

---
+++ modules/osticket/init.pp
@@ -0,0 +1,20 @@
+class osticket {	
+	file { '/etc/php/7.0/fpm/php.ini':
+        owner   => 'root',
+        group   => 'root',
+        content => template('/srv/puppet/files/php.ini'),
+	}
+
+	# reload php7.0-fpm
+	exec { 'php7 reload' :
+		command => '/usr/sbin/service php7.0-fpm reload',
+	}
+	
+	# Make directory for osticket
+	file { '/var/www/html/osticket':
+		ensure => 'directory',
+		owner  => 'www-data',
+		group  => 'www-data',
+		mode   => '0755',
+	}
+}
