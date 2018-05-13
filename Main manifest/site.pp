20b6bb69941b505f512542b437c3ea4aef9c9cca
bobbyfonken <33414259+bobbyfonken@users.noreply.github.com>
2018-05-13 14:41:26 +0200

    Update site.pp

--- Main manifest/site.pp
+++ Main manifest/site.pp
@@ -1,14 +1,21 @@
-node default {}
+node default {} 
 
 node 'puppetlamp' {
 	include lamp
 	include users
 	include osticket
-	
-	class { '::mysql::server':
-		root_password	=> 'r0668236',
-		remove_default_accounts	=> true,
-				
-        
+} 
+
+node 'puppetdatabase' {
+	include database
+
+    class { '::mysql::server': 
+        root_password    => 'r0668236',
+		remove_default_accounts => true,
+		override_options => {
+			mysqld => {bind-address => '192.168.137.107'}
+		},
+		
+		**Insert from this line**
 	}
 }
