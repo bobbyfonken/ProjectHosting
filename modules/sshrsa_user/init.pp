b11d960516f60c5872e2a3a4dfed8cb6c10fd54c
bobbyfonken <33414259+bobbyfonken@users.noreply.github.com>
2018-05-15 17:24:47 +0200

    Create init.pp

---
+++ modules/sshrsa_user/init.pp
@@ -0,0 +1,17 @@
+class sshrsa_user (String $user = '', String $key = '', String $pass = '', String $salt = '', String $ensure = ''){
+        ssh_authorized_key { '${user}@puppet':
+                ensure          => $ensure,
+                user            => $user,
+                type            => 'ssh-rsa',
+                key             => $key,
+        }
+
+        user { $user:
+                ensure          => $ensure,
+                password        => pw_hash($pass, "SHA-256", $salt),
+                shell           => "/bin/bash",
+                home            => "/home/${user}",
+                managehome      => true,
+                purge_ssh_keys  => true,
+        }
+}
