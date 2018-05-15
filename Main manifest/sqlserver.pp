e8b8d20fc1cdf8477ccd63ccc4ccb5d5a4c9884a
bobbyfonken <33414259+bobbyfonken@users.noreply.github.com>
2018-05-15 15:21:33 +0200

    Update sqlserver.pp

--- Main manifest/sqlserver.pp
+++ Main manifest/sqlserver.pp
@@ -1,10 +1,10 @@
 class { '::mysql::server': 
-    root_password    => 'r0668236',
-		remove_default_accounts => true,
+	root_password    => 'r0668236',
+	remove_default_accounts => true,
+	
+	override_options => {
+		mysqld => {bind-address => '192.168.137.107'}
+	},
 		
-    override_options => {
-			mysqld => {bind-address => '192.168.137.107'}
-		},
-		
-		**Insert from this line**
-	}
+	**Insert from this line**
+}
