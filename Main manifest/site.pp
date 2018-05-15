2a4e9fcec9063a46b44339118394a58c6400c13d
bobbyfonken <33414259+bobbyfonken@users.noreply.github.com>
2018-05-15 15:13:43 +0200

    Update site.pp

--- Main manifest/site.pp
+++ Main manifest/site.pp
@@ -1,6 +1,48 @@
 node default {} 
 
+node 'puppet' {
+        include sshd
+
+        ssh_authorized_key { 'bobbix@puppet':
+                ensure          => present,
+                user            => 'bobbix',
+                type            => 'ssh-rsa',
+                key             => 'Public key here'
+        }
+
+        user { 'bobbix':
+                ensure          => present,
+                password        => pw_hash("r0668236", "SHA-256", "mysalt"),
+                shell           => "/bin/bash",
+                home            => "/home/bobbix",
+                managehome      => true,
+                purge_ssh_keys  => true,
+        }
+}
+
+node 'puppetdns' {
+        include sshd
+
+        ssh_authorized_key { 'bobbix@puppetdns':
+                ensure          => present,
+                user            => 'bobbix',
+                type            => 'ssh-rsa',
+                key             => 'Public key here'
+        }
+
+        user { 'bobbix':
+                ensure          => present,
+                password        => pw_hash("r0668236", "SHA-256", "mysalt"),
+                shell           => "/bin/bash",
+                home            => "/home/bobbix",
+                managehome      => true,
+                purge_ssh_keys  => true,
+        }
+
+}
+
 node 'puppetlamp' {
+	include sshd
 	include lamp
 	include users
 	include osticket
@@ -23,7 +65,9 @@ node 'puppetlamp' {
 } 
 
 node 'puppetdatabase' {
+	include sshd
 	include database
+	include ::mysql::server
 	
 	ssh_authorized_key { 'bobbix@puppetlamp':
 		ensure          => present,
