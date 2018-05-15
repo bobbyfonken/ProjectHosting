ffcf584768365adf0eb900f237acecbffd2a2476
bobbyfonken <33414259+bobbyfonken@users.noreply.github.com>
2018-05-15 15:08:57 +0200

    Update site.pp

--- Main manifest/site.pp
+++ Main manifest/site.pp
@@ -40,14 +40,4 @@ node 'puppetdatabase' {
 		managehome      => true,
 		purge_ssh_keys  => true,
 	}
-
-	class { '::mysql::server': 
-		root_password    => 'r0668236',
-		remove_default_accounts => true,
-		override_options => {
-			mysqld => {bind-address => '192.168.137.107'}
-		},
-		
-		**Insert from this line**
-	}
 }
