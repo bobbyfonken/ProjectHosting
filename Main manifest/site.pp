0d9c535f970c304615afb70779c2f4461b7d9d69
bobbyfonken <33414259+bobbyfonken@users.noreply.github.com>
2018-05-15 19:43:48 +0200

    Update site.pp

--- Main manifest/site.pp
+++ Main manifest/site.pp
@@ -38,27 +38,4 @@ node 'puppetlamp' {
 		salt    => 'mysalt',
 		ensure  => present,
 	}
-} 
-
-node 'puppetdatabase' {
-	include sshd
-	include database
-	
-	class {'sshd::user':
-		user    => 'Bobbix',
-		key     => 'Public key here',
-		pass    => 'r0668236',
-		salt    => 'mysalt',
-		ensure  => present,
-	}
-	
-	class { '::mysql::server': 
-	root_password    => 'r0668236',
-		remove_default_accounts => true,
-		override_options => {
-			mysqld => {bind-address => '192.168.137.107'}
-		},
-		
-		**Insert from this line**
-	}
 }
