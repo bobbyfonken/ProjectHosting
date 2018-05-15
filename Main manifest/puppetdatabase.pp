98850c2c3b6d311255705ef1181478ebd9ccda25
bobbyfonken <33414259+bobbyfonken@users.noreply.github.com>
2018-05-15 19:44:26 +0200

    Create puppetdatabase.pp

---
+++ Main manifest/puppetdatabase.pp
@@ -0,0 +1,22 @@
+node 'puppetdatabase' {
+	include sshd
+	include database
+	
+	class {'sshd::user':
+		user    => 'Bobbix',
+		key     => 'Public key here',
+		pass    => 'r0668236',
+		salt    => 'mysalt',
+		ensure  => present,
+	}
+	
+	class { '::mysql::server': 
+	root_password    => 'r0668236',
+		remove_default_accounts => true,
+		override_options => {
+			mysqld => {bind-address => '192.168.137.107'}
+		},
+		
+		**Insert from this line**
+	}
+}
