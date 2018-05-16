4c1b06ac55e100c6136b8a1a5e5397963459dc52
bobbyfonken <33414259+bobbyfonken@users.noreply.github.com>
2018-05-16 14:24:20 +0200

    Update init.pp

--- modules/lamp/init.pp
+++ modules/lamp/init.pp
@@ -89,12 +89,4 @@ class lamp {
 		require => Package['vsftpd'], 
 		content => template('/srv/puppet/files/vsftpd.chroot_list'), 
 	}
-	
-	# Make directory for vsftpd certificates and keys
-	file { '/etc/ssl/vsftpd':
-		ensure => 'directory',
-		owner  => 'root',
-		group  => 'ssl-cert',
-		mode   => '0710',
-	}
 } 
