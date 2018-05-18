d62b6d33e5b4eca5ed03b5e1086a1d96496affaf
bobbyfonken <33414259+bobbyfonken@users.noreply.github.com>
2018-05-18 14:22:05 +0200

    Update init.pp

--- modules/lamp/init.pp
+++ modules/lamp/init.pp
@@ -51,32 +51,6 @@ class lamp {
 		command => '/usr/sbin/service apache2 reload', 
 	}
 	
-	# install phpmyadmin 
-	package { 'phpmyadmin' : 
-		ensure => latest, 
-	} 
-	
-	package { 'php-mbstring' : 
-		ensure => latest, 
-	} 
-	
-	package { 'php-gettext' : 
-		ensure => latest, 
-	} 
-	
-	# Comment volgende exec uit van zodra het 1 keer is uitgevoerd en phpmyadmin werkende is! 
-	exec { 'apache2 phpmyadmin' : 
-		command => '/bin/ln -s /etc/phpmyadmin/apache.conf /etc/apache2/conf-available/phpmyadmin.conf', 
-	} 
-	
-	exec { 'apache2 phpmyadmin 2' : 
-		command => '/usr/sbin/a2enconf phpmyadmin.conf', 
-	} 
-	
-	exec { 'apache2 phpmyadmin reload' : 
-		command => '/usr/sbin/service apache2 reload', 
-	} 
-	
 	# install vsftpd and configure 
 	package { 'vsftpd' : 
 		ensure => latest, 
