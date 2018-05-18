d39418a3ca07f9aabaafdfd9a6be30d5b071c903
bobbyfonken <33414259+bobbyfonken@users.noreply.github.com>
2018-05-18 18:09:18 +0200

    Update init.pp

--- modules/database/init.pp
+++ modules/database/init.pp
@@ -30,6 +30,25 @@ class database {
 		ensure => latest, 
 	} 
 	
+	# file to hide sensitive information
+        file { '/etc/apache2/conf-available/custom.conf':
+                notify  => Service['apache2'],
+                owner   => 'root',
+                group   => 'root',
+                require => Package['apache2'],
+                content => template('/srv/puppet/files/custom.conf'),
+        }
+
+       # custom apache2 config 2
+        exec { 'a2enmod headers' :
+                command => '/usr/sbin/a2enmod headers',
+        }
+
+       # custom apache2 config 3
+        exec { 'a2enconf custom.conf' :
+                command => '/usr/sbin/a2enconf custom.conf',
+        }
+	
 	exec { 'apache2 reload' : 
 		command => '/usr/sbin/service apache2 reload', 
 	} 
