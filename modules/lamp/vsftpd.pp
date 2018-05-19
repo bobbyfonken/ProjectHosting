264c541a57424c0e00d511e8f4f17ae916392c6c
bobbyfonken <33414259+bobbyfonken@users.noreply.github.com>
2018-05-19 18:55:24 +0200

    Create vsftpd.pp

---
+++ modules/lamp/vsftpd.pp
@@ -0,0 +1,33 @@
+class lamp::vsftpd {
+# install vsftpd and configure 
+	package { 'vsftpd' : 
+		ensure => latest, 
+	} 
+	
+	#ensure vsftpd service is running 
+	service { 'vsftpd' : 
+		ensure => running, 
+		require => Package['vsftpd'], 
+	} 
+	
+	#execute 'apt-get update' 
+	exec { 'apt-update 2' : 
+		command => '/usr/bin/apt-get update', 
+	} 
+	
+	file { '/etc/vsftpd.conf': 
+		notify  => Service['vsftpd'], 
+		owner   => 'root', 
+		group   => 'root', 
+		require => Package['vsftpd'], 
+		content => template('/srv/puppet/files/vsftpd.conf'), 
+	} 
+	
+	file { '/etc/vsftpd.chroot_list': 
+		notify  => Service['vsftpd'], 
+		owner   => 'root', 
+		group   => 'root', 
+		require => Package['vsftpd'], 
+		content => template('/srv/puppet/files/vsftpd.chroot_list'), 
+	}
+}
