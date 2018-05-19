77d89bf1a6604a9fd68c770ab0d6cc19dea0b488
bobbyfonken <33414259+bobbyfonken@users.noreply.github.com>
2018-05-19 18:53:59 +0200

    Update init.pp

--- modules/lamp/init.pp
+++ modules/lamp/init.pp
@@ -1,15 +1,4 @@
-class lamp { 
-	#execute 'apt-get update' 
-		exec { 'apt-update' : 
-		command => '/usr/bin/apt-get update', 
-		before => Package['apache2'],
-	} 
-	
-	#execute 'apt-get upgrade'
-		exec { 'apt-upgrade' :
-		command => '/usr/bin/apt-get upgrade -y',
-	}
-	
+class lamp { 	
 	#install apache2 package 
 	package { 'apache2' : 
 		ensure => latest, 
@@ -30,32 +19,23 @@ class lamp {
 		ensure => latest, 
 	} 
 	
-	# custom apache2 config 
-	exec { 'a2enmod userdir' : 
-		command => '/usr/sbin/a2enmod userdir', 
-	} 
-	
 	# file to hide sensitive information
-        file { '/etc/apache2/conf-available/custom.conf':
-                notify  => Service['apache2'],
-                owner   => 'root',
-                group   => 'root',
-                require => Package['apache2'],
-                content => template('/srv/puppet/files/custom.conf'),
-        }
+	file { '/etc/apache2/conf-available/custom.conf':
+		notify  => Service['apache2'],
+		owner   => 'root',
+		group   => 'root',
+		require => Package['apache2'],
+		content => template('/srv/puppet/files/custom.conf'),
+	}
 
-       # custom apache2 config 2
-        exec { 'a2enmod headers' :
-                command => '/usr/sbin/a2enmod headers',
-        }
+	# custom apache2 config 2
+	exec { 'a2enmod headers' :
+		command => '/usr/sbin/a2enmod headers',
+	}
 
-       # custom apache2 config 3
-        exec { 'a2enconf custom.conf' :
-                command => '/usr/sbin/a2enconf custom.conf',
-        }
-	
-	exec { 'apache2 reload' : 
-		command => '/usr/sbin/service apache2 reload', 
+	# custom apache2 config 3
+	exec { 'a2enconf custom.conf' :
+		command => '/usr/sbin/a2enconf custom',
 	} 
 	
 	file { '/etc/apache2/mods-enabled/php7.0.conf': 
@@ -66,39 +46,7 @@ class lamp {
 		content => template('/srv/puppet/files/php7.0.conf'), 
 	} 
 	
-	exec { 'apache2 reload 2' : 
+	exec { 'apache reload' : 
 		command => '/usr/sbin/service apache2 reload', 
-	}
-	
-	# install vsftpd and configure 
-	package { 'vsftpd' : 
-		ensure => latest, 
-	} 
-	
-	#ensure vsftpd service is running 
-	service { 'vsftpd' : 
-		ensure => running, 
-		require => Package['vsftpd'], 
-	} 
-	
-	#execute 'apt-get update' 
-	exec { 'apt-update 2' : 
-		command => '/usr/bin/apt-get update', 
-	} 
-	
-	file { '/etc/vsftpd.conf': 
-		notify  => Service['vsftpd'], 
-		owner   => 'root', 
-		group   => 'root', 
-		require => Package['vsftpd'], 
-		content => template('/srv/puppet/files/vsftpd.conf'), 
-	} 
-	
-	file { '/etc/vsftpd.chroot_list': 
-		notify  => Service['vsftpd'], 
-		owner   => 'root', 
-		group   => 'root', 
-		require => Package['vsftpd'], 
-		content => template('/srv/puppet/files/vsftpd.chroot_list'), 
 	}
 } 
