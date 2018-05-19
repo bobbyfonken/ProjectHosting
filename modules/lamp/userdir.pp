f81923801b53d1a8148d4bc0a0fa5d534949f2b4
bobbyfonken <33414259+bobbyfonken@users.noreply.github.com>
2018-05-19 20:42:42 +0200

    Update userdir.pp

--- modules/lamp/userdir.pp
+++ modules/lamp/userdir.pp
@@ -4,13 +4,17 @@ class lamp::userdir {
 		command => '/usr/sbin/a2enmod userdir', 
 	}
 	
-	file { '/etc/apache2/mods-enabled/php7.0.conf': 
+	file { '/etc/apache2/mods-available/php7.0.conf': 
 		notify  => Service['apache2'], 
 		owner   => 'root', 
 		group   => 'root', 
 		require => Package['apache2'], 
 		content => template('/srv/puppet/files/php7.0.conf'), 
 	} 
+
+	exec { 'a2enmod php7.0' :
+		command => '/usr/sbin/a2enmod php7.0',
+	}
 	
 	exec { 'apache reload 3' : 
 		command => '/usr/sbin/service apache2 reload', 
