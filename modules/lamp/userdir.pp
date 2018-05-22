7b6d6206802352a6dd5328b2e9340c2278efcd58
bobbyfonken <33414259+bobbyfonken@users.noreply.github.com>
2018-05-22 09:43:04 +0200

    Update userdir.pp

--- modules/lamp/userdir.pp
+++ modules/lamp/userdir.pp
@@ -1,9 +1,10 @@
 class lamp::userdir {
-	# custom apache2 config 
+	# enable to use users home directory ~/public_html for apache
 	exec { 'a2enmod userdir' : 
 		command => '/usr/sbin/a2enmod userdir', 
 	}
 	
+	# enable the use of PHP in that directory
 	file { '/etc/apache2/mods-available/php7.0.conf': 
 		notify  => Service['apache2'], 
 		owner   => 'root', 
@@ -12,10 +13,12 @@ class lamp::userdir {
 		content => template('/srv/puppet/files/php7.0.conf'), 
 	} 
 
+	# enable the configuration
 	exec { 'a2enmod php7.0' :
 		command => '/usr/sbin/a2enmod php7.0',
 	}
 	
+	# reload apache to activate changes
 	exec { 'apache reload 3' : 
 		command => '/usr/sbin/service apache2 reload', 
 	}
