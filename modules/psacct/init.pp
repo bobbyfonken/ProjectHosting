0e52d8ff37e497f63498896a60d54f03f2693d47
bobbyfonken <33414259+bobbyfonken@users.noreply.github.com>
2018-05-22 11:15:03 +0200

    Create init.pp

---
+++ modules/psacct/init.pp
@@ -0,0 +1,12 @@
+class psacct {
+        #install psact package
+        package { 'acct' :
+                ensure => latest,
+        }
+
+        #ensure psact service is running
+        service { 'acct' :
+                ensure => running,
+                require => Package['acct'],
+        }
+}
