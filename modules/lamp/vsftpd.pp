3c5a52a787a622f559c54875c09e9b0336cedfd7
bobbyfonken <33414259+bobbyfonken@users.noreply.github.com>
2018-05-22 14:05:57 +0200

    Update vsftpd.pp

--- modules/lamp/vsftpd.pp
+++ modules/lamp/vsftpd.pp
@@ -1,48 +1,52 @@
-class lamp::vsftpd 
-(
-	String $port = '', 
-	String $umask = '', 
-	String $vsftpdserverkey = '', 
+class lamp::vsftpd (
+	String $port = '',
+	String $umask = '',
+	String $vsftpdserverkey = '',
 	String $vsftpdcertificate = '',
+	Array $freeusers = '',
 ){
-	# install vsftpd and configure 
-	package { 'vsftpd' : 
-		ensure => latest, 
+	# install vsftpd and configure
+	package { 'vsftpd' :
+		ensure => latest,
 	} 
 	
-	#ensure vsftpd service is running 
-	service { 'vsftpd' : 
-		ensure => running, 
-		require => Package['vsftpd'], 
+	#ensure vsftpd service is running
+	service { 'vsftpd' :
+		ensure => running,
+		require => Package['vsftpd'],
 	} 
 	
 	# vsftpd configuration
-	file { '/etc/vsftpd.conf': 
-		notify  => Service['vsftpd'], 
-		owner   => 'root', 
-		group   => 'root', 
-		require => Package['vsftpd'], 
+	file { '/etc/vsftpd.conf':
+		notify => Service['vsftpd'],
+		owner => 'root',
+		group => 'root',
+		require => Package['vsftpd'],
 		content => epp('/srv/puppet/files/vsftpd.conf.epp',
                 {
-                        'port'			=> $port,
-			'umask'			=> $umask,
-			'vsftpdserverkey'	=> $vsftpdserverkey,
-			'vsftpdcertificate'	=> $vsftpdcertificate,
+                        'port' => $port,
+			'umask' => $umask,
+			'vsftpdserverkey' => $vsftpdserverkey,
+			'vsftpdcertificate' => $vsftpdcertificate,
                 },
-                ), 
+                ),
 	} 
 	
 	# vsftpd chroot list (allowing these users to be free)
-	file { '/etc/vsftpd.chroot_list': 
-		notify  => Service['vsftpd'], 
-		owner   => 'root', 
-		group   => 'root', 
-		require => Package['vsftpd'], 
-		content => template('/srv/puppet/files/vsftpd.chroot_list'), 
+	file { '/etc/vsftpd.chroot_list':
+		notify => Service['vsftpd'],
+		owner => 'root',
+		group => 'root',
+		require => Package['vsftpd'],
+		content => epp('/srv/puppet/files/vsftpd.chroot_list.epp',
+                {
+                        'freeusers' => $freeusers,
+                },
+                ),
 	}
 
 	# reload vsftpd to activate configuration
-	exec { 'vsftpd reload' : 
-		command => '/usr/sbin/service vsftpd reload', 
+	exec { 'vsftpd reload' :
+		command => '/usr/sbin/service vsftpd reload',
 	}
 }
