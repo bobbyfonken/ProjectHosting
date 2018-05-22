b0aab186ca4bcedf96d2583e9e63b8ceebe8c9fe
bobbyfonken <33414259+bobbyfonken@users.noreply.github.com>
2018-05-22 09:50:58 +0200

    Update root.pp

--- modules/sshd/root.pp
+++ modules/sshd/root.pp
@@ -1,4 +1,5 @@
 class sshd::root (String $password = '', String $salt = ''){
+	# class for controlling the root accounts
 	user { 'root':
 		ensure          => present,
 		password        => pw_hash($password, "SHA-256", $salt),
