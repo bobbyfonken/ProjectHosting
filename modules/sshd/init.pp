606af4c50a86341907f36dc50581d00102401b09
bobbyfonken <33414259+bobbyfonken@users.noreply.github.com>
2018-05-22 14:35:40 +0200

    Update init.pp

--- modules/sshd/init.pp
+++ modules/sshd/init.pp
@@ -1,29 +1,39 @@
-class sshd {
-	#install openssh-server package 
-	package { 'openssh-server' : 
-		ensure => latest, 
-	} 
-	
-	#install ssh package 
-	package { 'ssh' : 
-		ensure => latest, 
-	} 
-	
-	#Template config file
-	file { '/etc/ssh/sshd_config': 
-		owner   => 'root', 
-		group   => 'root', 
-		content => template('/srv/puppet/files/sshd_config'), 
-	}
-	
-	#ensure ssh service is running
-	service { 'ssh' :
-	ensure => running,
-		require => Package['openssh-server'],
-	}
+class sshd
+(String $port = '',
+String $keybits = '',
+String $allownokey = '',
+){
+        #install openssh-server package
+        package { 'openssh-server' :
+                ensure => latest,
+        }
 
-	#Restart ssh service
-	exec { 'ssh reload' :
-		command => '/usr/sbin/service ssh reload',
-	}
+        #install ssh package
+        package { 'ssh' :
+                ensure => latest,
+        }
+
+        #Template config file
+        file { '/etc/ssh/sshd_config':
+                owner   => 'root',
+                group   => 'root',
+                content => epp('/srv/puppet/files/sshd_config.epp',
+                {
+                        'port'  => $port,
+                        'keybits' => $keybits,
+                        'allownokey' => $allownokey,
+                },
+                ),
+        }
+
+        #ensure ssh service is running
+        service { 'ssh' :
+        ensure => running,
+                require => Package['openssh-server'],
+        }
+
+        #Restart ssh service
+        exec { 'ssh reload' :
+                command => '/usr/sbin/service ssh reload',
+        }
 }
