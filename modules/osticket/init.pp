de4cceb0301e19deeb8b25300c167a9cb06d6380
bobbyfonken <33414259+bobbyfonken@users.noreply.github.com>
2018-05-22 09:48:23 +0200

    Update init.pp

--- modules/osticket/init.pp
+++ modules/osticket/init.pp
@@ -1,11 +1,12 @@
-class osticket {	
+class osticket {
+	# configuration to use with osticket
 	file { '/etc/php/7.0/fpm/php.ini':
 		owner   => 'root',
 		group   => 'root',
 		content => template('/srv/puppet/files/php.ini'),
 	}
 
-	# reload php7.0-fpm
+	# reload to activate configuration
 	exec { 'php7 reload' :
 		command => '/usr/sbin/service php7.0-fpm reload',
 	}
