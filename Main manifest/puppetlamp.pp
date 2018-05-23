ebaaaa73b55ecf896ba42449df47dc72e3c8be08
bobbyfonken <33414259+bobbyfonken@users.noreply.github.com>
2018-05-23 13:47:27 +0200

    Update puppetlamp.pp

--- Main manifest/puppetlamp.pp
+++ Main manifest/puppetlamp.pp
@@ -7,7 +7,13 @@ node 'puppetlamp' {
 	include psacct
 	include my_fw
 	
-	# default port used by puppet
+	firewall { '111 allow all from puppetdatabase':
+                source  => '192.168.137.107',
+                proto   => 'all',
+                action  => 'accept',
+        }
+
+        # default port used by puppet
         firewall { '112 open port 8140':
                 dport   => 8140,
                 proto   => tcp,
@@ -17,6 +23,13 @@ node 'puppetlamp' {
         # port number should be the same as the one from sshd below
         firewall { '113 open port 2222 sshd':
                 dport   => 2222,
+                proto   => tcp,
+                action  => 'accept',
+        }
+
+        # allow phpmyadmin
+        firewall { '114 open port 80 apache':
+                dport   => 80,
                 proto   => tcp,
                 action  => 'accept',
         }
