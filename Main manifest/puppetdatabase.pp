7a39ba99911b260c51c7cca09213e795f1c540ca
bobbyfonken <33414259+bobbyfonken@users.noreply.github.com>
2018-05-23 13:36:33 +0200

    Update puppetdatabase.pp

--- Main manifest/puppetdatabase.pp
+++ Main manifest/puppetdatabase.pp
@@ -5,19 +5,32 @@ node 'puppetdatabase' {
 	include psacct
 	include my_fw
 	
+	firewall { '111 allow all from puppetlamp':
+			source  => '192.168.137.105',
+			proto   => 'all',
+			action  => 'accept',
+	}
+		
 	# default port used by puppet
-        firewall { '112 open port 8140':
-                dport   => 8140,
-                proto   => tcp,
-                action  => 'accept',
-        }
+	firewall { '112 open port 8140':
+			dport   => 8140,
+			proto   => tcp,
+			action  => 'accept',
+	}
 
-        # port number should be the same as the one from sshd below
-        firewall { '113 open port 2222 sshd':
-                dport   => 2222,
-                proto   => tcp,
-                action  => 'accept',
-        }
+	# port number should be the same as the one from sshd below
+	firewall { '113 open port 2222 sshd':
+			dport   => 2222,
+			proto   => tcp,
+			action  => 'accept',
+	}
+
+	# allow phpmyadmin
+	firewall { '114 open port 80 apache':
+			dport   => 80,
+			proto   => tcp,
+			action  => 'accept',
+	}
 	
 	# This should be the same as the pma user, database, and password
 	class {'lamp::phpmyadmin':
