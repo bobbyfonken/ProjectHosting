e47c0d119c26f2c1ff2f9c2e4f7dd92e82e3716b
bobbyfonken <33414259+bobbyfonken@users.noreply.github.com>
2018-05-27 15:02:37 +0200

    Update init.pp

--- modules/my_fw/manifests/init.pp
+++ modules/my_fw/manifests/init.pp
@@ -1,6 +1,6 @@
 class my_fw {
 
-   # This class closes all ports en then open the basic ones
+   # This class closes all ports en then open the basic ones based on what you put in post.pp and pre.pp
    stage { 'fw_pre':  before  => Stage['main']; }
    stage { 'fw_post': require => Stage['main']; }
 
